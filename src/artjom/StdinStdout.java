125931a6c53aa22d308d74b0fba867bc549a41f8
Artjom Lell <artjom.lell@web.de>
2019-09-24 18:25:49 +0200

    added solution for Stdin Stdout challenge

---
+++ src/artjom/StdinStdout.java
@@ -0,0 +1,18 @@
+package artjom;
+
+import java.util.Scanner;
+
+public class StdinStdout {
+
+	public static void main(String[] args) {
+		final Scanner scan = new Scanner(System.in);
+		final int i = scan.nextInt();
+		final double d = scan.nextDouble();
+		scan.nextLine();
+		final String text = scan.nextLine();
+
+		System.out.println("String: " + text);
+		System.out.println("Double: " + d);
+		System.out.print("Int: " + i);
+	}
+}
